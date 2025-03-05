c3b28d26e7b0cf70893890071b99d8eeb16cc8c8
Jan Candalija <jcandalija.dam@institutcampalans.net>
2025-03-05 16:47:29 +0100

    add(api/result): Obtenir llistat i posar-lo dins d'una llista de Language

--- app/src/main/java/cat/jan/retrofitapi/MainActivity.kt
+++ app/src/main/java/cat/jan/retrofitapi/MainActivity.kt
@@ -1,14 +1,38 @@
 package cat.jan.retrofitapi
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import cat.jan.retrofitapi.API.retrofitService
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    var languages = emptyList<Language>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        getLanguages()
+    }
+
+    private fun getLanguages() {
+        CoroutineScope(Dispatchers.IO).launch {
+            val result = retrofitService.getLanguages()
+
+            if (result.isSuccessful) {
+                languages = result.body() ?: emptyList()
+            } else {
+                showError()
+            }
+        }
+    }
+
+    private fun showError() {
+        runOnUiThread {
+            Toast.makeText(this, "Error de la peticio", Toast.LENGTH_SHORT).show()
+        }
     }
 }
