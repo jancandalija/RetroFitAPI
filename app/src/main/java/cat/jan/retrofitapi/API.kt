bc0b960afc3f73c8ea7f3758a9c3ba3bedf79f8e
Jan Candalija <jcandalija.dam@institutcampalans.net>
2025-03-05 16:39:40 +0100

    add(api/service): Inicialitzar o crear servei de l'api

--- app/src/main/java/cat/jan/retrofitapi/API.kt
+++ app/src/main/java/cat/jan/retrofitapi/API.kt
@@ -6,6 +6,10 @@ import retrofit2.converter.gson.GsonConverterFactory
 object API {
     const val BASE_URL = "https://ws.detectlanguage.com/"
 
+    val retrofitService: ApiService by lazy {
+        getRetrofit().create(ApiService::class.java)
+    }
+
     private fun getRetrofit() : Retrofit {
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
