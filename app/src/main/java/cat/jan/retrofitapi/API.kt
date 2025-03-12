8c45f23d017c7ba6e3961e8cf654ed6bb1ee6577
Jan Candalija <jcandalija.dam@institutcampalans.net>
2025-03-12 16:02:52 +0100

    add(logs): Afegir OkHttp per fer logs

--- app/src/main/java/cat/jan/retrofitapi/API.kt
+++ app/src/main/java/cat/jan/retrofitapi/API.kt
@@ -1,5 +1,7 @@
 package cat.jan.retrofitapi
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -11,9 +13,15 @@ object API {
     }
 
     private fun getRetrofit() : Retrofit {
+        val logging = HttpLoggingInterceptor()
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+        val httpClient = OkHttpClient.Builder()
+        httpClient.addInterceptor(logging)
+
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
+            .client(httpClient.build())
             .build()
     }
 }
